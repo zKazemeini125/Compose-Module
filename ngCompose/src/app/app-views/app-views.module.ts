d6ebc4b4fac6001b0f7417be1812cc5cddc0b50a
zKazemeini125 <kazemeiniz125@gmail.com>
2024-01-03 13:31:39 +0330

    create Modules

---
+++ ngCompose/src/app/app-views/app-views.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AppViewsRoutingModule } from './app-views-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    AppViewsRoutingModule
+  ]
+})
+export class AppViewsModule { }
