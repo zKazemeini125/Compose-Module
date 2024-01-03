ee1fa39bdae8330a573a7109bc8f18a55b7c769a
zKazemeini125 <kazemeiniz125@gmail.com>
2024-01-03 14:02:20 +0330

    Import, export componenets

---
+++ ngCompose/src/app/elements/el-home/el-home.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-el-home',
+  templateUrl: './el-home.component.html',
+  styleUrl: './el-home.component.css'
+})
+export class ElHomeComponent {
+
+}
--- ngCompose/src/app/elements/elements.module.ts
+++ ngCompose/src/app/elements/elements.module.ts
@@ -2,13 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { ElementsRoutingModule } from './elements-routing.module';
-
+import { ElHomeComponent } from './el-home/el-home.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule,
-    ElementsRoutingModule
-  ]
+  declarations: [ElHomeComponent],
+  imports: [CommonModule, ElementsRoutingModule],
+  exports: [ElHomeComponent],
 })
-export class ElementsModule { }
+export class ElementsModule {}
