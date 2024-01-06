eca573cbd59500eda9dae2d6e6c82972066e0458
zKazemeini125 <kazemeiniz125@gmail.com>
2024-01-06 16:35:54 +0330

    elements done

--- ngCompose/src/app/collection/collection.module.ts
+++ ngCompose/src/app/collection/collection.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 
 import { CollectionRoutingModule } from './collection-routing.module';
 import { CollecHomeComponent } from './collec-home/collec-home.component';
+import { TableComponent } from './table/table.component';
 
 @NgModule({
-  declarations: [CollecHomeComponent],
+  declarations: [CollecHomeComponent, TableComponent],
   imports: [CommonModule, CollectionRoutingModule],
 })
 export class CollectionModule {}
