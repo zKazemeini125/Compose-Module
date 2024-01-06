cac800de811fa8010224548a2edb0da5814a67bb
zKazemeini125 <kazemeiniz125@gmail.com>
2024-01-06 15:35:59 +0330

    ng-content

--- ngCompose/src/app/elements/elements.module.ts
+++ ngCompose/src/app/elements/elements.module.ts
@@ -5,9 +5,11 @@ import { ElementsRoutingModule } from './elements-routing.module';
 import { ElHomeComponent } from './el-home/el-home.component';
 import { PlaceholderComponent } from './placeholder/placeholder.component';
 import { RepeaterDirective } from './repeater.directive';
+import { DividerComponent } from '../shared/divider/divider.component';
+import { SegmentComponent } from './segment/segment.component';
 
 @NgModule({
-  declarations: [ElHomeComponent, PlaceholderComponent, RepeaterDirective],
-  imports: [CommonModule, ElementsRoutingModule],
+  declarations: [ElHomeComponent, PlaceholderComponent, RepeaterDirective, SegmentComponent],
+  imports: [CommonModule, ElementsRoutingModule, DividerComponent],
 })
 export class ElementsModule {}
