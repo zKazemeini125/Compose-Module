d7444cfa0a937259fa694ee2c389c58469035d50
zKazemeini125 <kazemeiniz125@gmail.com>
2024-01-08 17:42:28 +0330

    customize tab component

--- ngCompose/src/app/app.module.ts
+++ ngCompose/src/app/app.module.ts
@@ -1,21 +1,20 @@
-import { NgModule } from '@angular/core';
+import { NgModule } from "@angular/core";
 import {
   BrowserModule,
   provideClientHydration,
-} from '@angular/platform-browser';
+} from "@angular/platform-browser";
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { ElementsModule } from './elements/elements.module';
-import { CollectionModule } from './collection/collection.module';
-import { AppViewsModule } from './app-views/app-views.module';
-import { ModsModule } from './mods/mods.module';
-import { HomeComponent } from './home/home.component';
-import { NotFoundComponent } from './not-found/not-found.component';
-import { StatisticComponent } from './statistic/statistic.component';
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { ElementsModule } from "./elements/elements.module";
+import { CollectionModule } from "./collection/collection.module";
+import { AppViewsModule } from "./app-views/app-views.module";
+import { ModsModule } from "./mods/mods.module";
+import { HomeComponent } from "./home/home.component";
+import { NotFoundComponent } from "./not-found/not-found.component";
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, NotFoundComponent, StatisticComponent],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
   imports: [
     BrowserModule,
     //ElementsModule,
