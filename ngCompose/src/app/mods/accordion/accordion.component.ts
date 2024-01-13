4e91dd63b562e217b786ccb0f3060270b2129dfa
zKazemeini125 <kazemeiniz125@gmail.com>
2024-01-13 15:54:28 +0330

    Accordion added

---
+++ ngCompose/src/app/mods/accordion/accordion.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-accordion',
+  templateUrl: './accordion.component.html',
+  styleUrl: './accordion.component.css'
+})
+export class AccordionComponent {
+
+}
--- ngCompose/src/app/mods/mods.module.ts
+++ ngCompose/src/app/mods/mods.module.ts
@@ -5,9 +5,10 @@ import { ModsRoutingModule } from "./mods-routing.module";
 import { ModuleHomeComponent } from "./module-home/module-home.component";
 import { ModalComponent } from "./modal/modal.component";
 import { SharedModule } from "../shared/shared.module";
+import { AccordionComponent } from './accordion/accordion.component';
 
 @NgModule({
-  declarations: [ModuleHomeComponent, ModalComponent],
+  declarations: [ModuleHomeComponent, ModalComponent, AccordionComponent],
   imports: [CommonModule, ModsRoutingModule, SharedModule],
 })
 export class ModsModule {}
