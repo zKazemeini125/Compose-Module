cf0b3c247d62c1ab84a54a10c89eca6af3c3fc14
zKazemeini125 <kazemeiniz125@gmail.com>
2024-02-11 16:40:33 +0330

    edit searchbar

--- ngCompose/src/app/services/service-home/service-home.component.ts
+++ ngCompose/src/app/services/service-home/service-home.component.ts
@@ -1,10 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, Input } from "@angular/core";
 
 @Component({
-  selector: 'app-service-home',
-  templateUrl: './service-home.component.html',
-  styleUrl: './service-home.component.css'
+  selector: "app-service-home",
+  templateUrl: "./service-home.component.html",
+  styleUrl: "./service-home.component.css",
 })
 export class ServiceHomeComponent {
-
+  @Input() InputValue: string = "";
 }
