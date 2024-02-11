85db4b49604f46c21d4f8fa617701febd094556e
zKazemeini125 <kazemeiniz125@gmail.com>
2024-02-11 17:03:22 +0330

    add page list

--- ngCompose/src/app/services/services.module.ts
+++ ngCompose/src/app/services/services.module.ts
@@ -5,9 +5,10 @@ import { ServicesRoutingModule } from "./services-routing.module";
 import { ServiceHomeComponent } from "./service-home/service-home.component";
 import { SharedModule } from "../shared/shared.module";
 import { SearchbarComponent } from './searchbar/searchbar.component';
+import { PageListComponent } from './page-list/page-list.component';
 
 @NgModule({
-  declarations: [ServiceHomeComponent, SearchbarComponent],
+  declarations: [ServiceHomeComponent, SearchbarComponent, PageListComponent],
   imports: [CommonModule, ServicesRoutingModule, SharedModule],
 })
 export class ServicesModule {}
